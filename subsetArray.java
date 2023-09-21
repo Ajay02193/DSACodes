8018642231285f698d488400bb6c93915c2504e4
Ajay02193 <ajay02193@gmail.com>
2023-09-21 19:03:25 +0530

    check

---
+++ subsetArray.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class subsetArray {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Array size: ");
+        int n=scn.nextInt();
+        System.out.println("Array elements: ");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        subSet(arr,n);
+        scn.close();
+    }
+
+    public static void subSet(int[] arr, int n){
+        
+        int ts=1;
+        for(int i=1;i<=n;i++)
+            ts=ts*2;
+        System.out.println("Subsets of given array is: ");
+        for(int i=0;i<ts;i++)
+        {
+            int[] bin=getBinary(i,n);
+
+            for(int j=0;j<n;j++){
+                if(bin[j]==0)
+                    System.out.print("- ");
+                else
+                    System.out.print(arr[j]+" ");
+            }
+            System.out.println(" ");
+        }    
+    }
+
+    public static int[] getBinary(int i, int n){
+        int[] arr=new int[n];
+        int j=n-1;
+        while(i>0)
+        {       
+            int rem=i%2;
+            i=i/2;
+            arr[j]=rem;
+            j--;
+
+        }
+        return arr;
+    }
+}
